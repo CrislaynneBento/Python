79acee46082f74298f769ee1b52de289298240ae
Cris <crisbento@alu.ufc.br>
2026-04-19 01:18:20 -0300

    Delete env.py

--- env.py
+++
@@ -1,3 +0,0 @@
-#informações sensíveis e configurações importantes da aplicação. 
-#Esse arquivo nunca deve ser disponibilizado em repositórios públicos ou distribuído na internet.
-MONGO_URI=mongodb://127.0.0.1:27017//products
